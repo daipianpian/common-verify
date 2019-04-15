dba45959f11f01e566a7f3ec10c3cce76b3ae449
daipianpian <daipianpian@outlook.com>
2019-04-15 14:13:29 +0800

    1.0.3

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = require('./lib/verify.js');
+
+
+
+/*======== 可验证的有 ========*/
+/*
+1. 名称，需为字母或数字 
+   方法名：checkName
+2. 11位正确的手机号
+   方法名：checkMobile
+3. 0~100的整数或小数，包括0和100,小数点后最多只能保留两位
+   方法名：oneTohundredNum
+4. 大于0的数字，小数点后最多只能保留两位
+   方法名：positiveNum
+5. 大于等于0的整数
+   方法名：integerZeroInt
+6. 大于等于0的数字，小数点后最多只能保留两位
+   方法名：integerZeroFloat
+ */
+
+
+
+
+/*======== 使用方法 ========*/
+/*
+1. cnpm install --save vue-common-verify
+2. 在main.js里引入：import vueCommonVerify from 'vue-common-verify'
+	而后 Vue.prototype.vueCommonVerify = vueCommonVerify
+3. 项目中使用
+	// 验证手机号
+	let data = {
+		eventName: 'checkMobile', // 检验时，传入的检验名称（必传）
+		value: 13278123456,  // 检验时，传入的检验值（需传）
+		msgSuccess: '验证手机号正确',  // 检验成功时，返回的信息（不必传）
+		msgFail: '验证手机号失败'  // 检验失败时，返回的信息（不必传）
+	}
+	let result = this.vueCommonVerify.verify(data)
+	console.log('result==='+JSON.stringify(result))
+ */
---
+++ src/verify.js
@@ -0,0 +1,104 @@
+// 验证结果
+function npmReturnMsg(regParam, value, msgSuccess, msgFail) {
+	let reg = regParam.rule;
+	let result = {};
+	if(!value){
+		result = {
+			data: false,
+			message: !msgFail?'输入的值不能为空！':msgFail
+		};
+	}else{
+		if (reg.test(value)) {
+			result = {
+				data: true,
+				message: !msgSuccess?'验证成功！':msgSuccess
+			};
+		}else{
+			result = {
+				data: false,
+				message: !msgFail?regParam.msg:msgFail
+			};
+		}
+	}
+	return result;
+}
+
+// 各种验证
+exports.verify = function(data){
+	if(data instanceof Array){
+		let result = {
+			data: false,
+			message: '请传入正确的值类型！'
+		};
+        return result;
+    }else if(data instanceof Object){
+		let eventName = data.eventName;  // 检验时，传入的检验名称
+		let value = data.value;  // 检验时，传入的检验值
+		let msgSuccess = !data.msgSuccess?null:data.msgSuccess;  // 检验成功时，返回的信息
+		let msgFail = !data.msgFail?null:data.msgFail;  // 检验失败时，返回的信息
+		if(!eventName){
+			let result = {
+				data: false,
+				message: '请传入正确的检验名称'
+			};
+	        return result;
+		}else if(eventName == 'checkName'){
+			// 验证名字，只能输入字母或数字
+			const regParam = {
+				rule: /^[0-9a-zA-Z]*$/g,
+				msg: '名称需为字母或数字'
+			};
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else if(eventName == 'checkMobile'){
+			// 验证11位手机号
+			const regParam = {
+				rule: /^[\d]{11}$/,
+				msg: '请输入11位正确的手机号'
+			};
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else if(eventName == 'oneTohundredNum'){
+			// 0~100的整数或小数，包括0和100,小数点后最多只能保留两位
+			const regParam = {
+				rule: /^(\d|[1-9]\d|100)(\.\d{1,2})?$/,
+				msg: '请输入0~100的整数或小数，包括0和100,小数点后最多只能保留两位'
+			};
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else if(eventName == 'positiveNum'){
+			// 只能输入大于0的数字，小数点后最多只能保留两位
+			const regParam = {
+				rule: /^[1-9]{1}\d*(.\d{1,2})?$|^0.\d{1,2}$/,
+				msg: '只能输入大于0的数字，小数点后最多只能保留两位'
+			}
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else if(eventName == 'integerZeroInt'){
+			// 大于等于0的整数
+			const regParam = {
+				rule: /^(0|[1-9][0-9]*)$/,
+				msg: '请输入大于等于0的整数'
+			};
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else if(eventName == 'integerZeroFloat'){
+			// 大于等于0的数字，小数点后最多只能保留两位
+			const regParam = {
+				rule: /^\d+(\.\d{1,2})?$/,
+				msg: '请输入大于等于0的数字，小数点后最多只能保留两位'
+			};
+			return npmReturnMsg(regParam, value, msgSuccess, msgFail);
+		}else{
+			let result = {
+				data: false,
+				message: '暂无匹配的校验函数'
+			};
+	        return result;
+		}
+    }else{
+        let result = {
+			data: false,
+			message: '请传入正确的值类型！'
+		};
+        return result;
+    }
+}
+
+
+
